8c4522ac4de81f678db2aa748f88a7eb0eeaffb5
vinutha1621 <vinuthareddy27@gmail.com>
2026-07-26 05:39:57 +0000

    updated nat gateway and route tables

---
+++ terraform/nat-gateway.tf
@@ -0,0 +1,23 @@
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.project_name}-eip"
+  }
+}
+
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_1.id
+
+  tags = {
+    Name = "${var.project_name}-nat"
+  }
+
+  depends_on = [
+    aws_internet_gateway.igw
+  ]
+}
+
+
+
